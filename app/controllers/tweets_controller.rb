5e3e163655287f7f10291e44b5c2dfbfe2dae031
Austin French <austindanielfrench@gmail.com>
2025-09-07 10:04:12 -0700

    controller dispatch to action working

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,6 @@
+class TweetsController < Tinyrails::Controller
+  def a_tweet
+    "There is nothing either good or bad " + 
+      "but thinking makes it so."
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -2,5 +2,7 @@ require "tinyrails"
 
 module BestTweets
   class Application < Tinyrails::Application
+    $LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+    require "tweets_controller"
   end
 end
