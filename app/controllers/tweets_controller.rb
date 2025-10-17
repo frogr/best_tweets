76f97bcdc455b873fb2ee23239a3a6a9260acf00
Austin French <austindanielfrench@gmail.com>
2025-10-16 21:59:07 -0700

    added tweet_1 and index routes

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,14 @@ class TweetsController < Tinyrails::Controller
   def a_tweet
     render :a_tweet, :noun => :winking
   end
+
+  def tweet_1
+    tweet_1 = FileModel.find(1)
+    render :tweet, :obj => tweet_1
+  end
+
+  def index
+    tweets = FileModel.all
+    render :index, :tweets => tweets
+  end
 end
